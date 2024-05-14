84362450cc5428a150117e7e77f4de76c705d8d6
AriaPramesta <pramestaaria@gmail.com>
2024-05-14 19:34:29 +0700

    feat: add endpoint for adding new writers

---
+++ src/data/writers.ts
@@ -0,0 +1,31 @@
+type writer = {
+  id: number;
+  name: string;
+  birthDate: string;
+  country: string;
+  popularBook: string;
+};
+
+export const dataWriters: writer[] = [
+  {
+    id: 1,
+    name: "Kahlil Gibran",
+    birthDate: "6-January-1883",
+    country: "America",
+    popularBook: "The Prophet",
+  },
+  {
+    id: 2,
+    name: "Franz Kafka",
+    birthDate: "3-July-1883",
+    country: "Austria",
+    popularBook: "The Metamorphosis",
+  },
+  {
+    id: 3,
+    name: "Pramoedya Ananta Toer",
+    birthDate: "6-February-1925",
+    country: "Indonesia",
+    popularBook: "Tetralogi Buru",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+import { dataWriters } from "./data/writers";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+let writers = dataWriters;
 
-export default app
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({ message: "Welcome to The Writers!" });
+});
+
+app.get("/writers", (c) => {
+  return c.json(writers);
+});
+
+app.get("/writers/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  const writer = writers.find((writer) => writer.id === id);
+
+  if (!writer) {
+    c.status(404);
+    return c.json({ message: "Writer not found" });
+  }
+
+  return c.json(writer);
+});
+
+app.post("/writers", async (c) => {
+  const body = await c.req.json();
+
+  const nextId = writers[writers.length - 1].id + 1;
+
+  const newWriter = {
+    id: nextId,
+    name: body.name,
+    birthDate: body.birthDate,
+    country: body.country,
+    popularBook: body.popularBook,
+  };
+
+  writers = [...writers, newWriter];
+
+  return c.json({ writer: newWriter });
+});
+
+export default app;
