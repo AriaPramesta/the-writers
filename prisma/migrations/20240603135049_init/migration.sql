a4594ae78557c21497229f0d99617448bd8d7592
Aria Adi Pramesta <pramestaaria@gmail.com>
2024-06-03 20:56:30 +0700

    feat: setup prisma with migration

---
+++ prisma/migrations/20240603135049_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Writer" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Writer_pkey" PRIMARY KEY ("id")
+);
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,10 +3,11 @@ import * as pg from "pg";
 
 const { Client } = pg;
 export const client = new Client({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: Number(process.env.POSTGRES_PORT),
-  //   connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+
+  //   host: process.env.POSTGRES_HOST,
+  //   user: process.env.POSTGRES_USER,
+  //   password: process.env.POSTGRES_PASSWORD,
+  //   database: process.env.POSTGRES_DB,
+  //   port: Number(process.env.POSTGRES_PORT),
 });
